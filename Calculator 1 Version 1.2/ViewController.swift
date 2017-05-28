3b2d97490c712007816427435855bcd22274515a
Viet Anh Doan <vtahdn@icloud.com>
2017-05-28 22:23:54 +0700

    cm

--- Calculator 1 Version 1.2/ViewController.swift
+++ Calculator 1 Version 1.2/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var botConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        let scale = UIScreen.main.bounds.height / 671
+        topConstraint.constant *= scale
+        botConstraint.constant *= scale
+    }
 }
 
